37c46d053ff272f3af89a18b5fdbca62646c87af
Evan Seils <evanseils2013@gmail.com>
2014-10-08 12:08:22 -0500

    Logic Improvements

--- YoBitcoins.py
+++ YoBitcoins.py
@@ -1,27 +1,38 @@
 #
-#Author: Evan Seils
 #
-#Version: 1.0
+# Author: Evan Seils
 #
-#Please excuse any bugs that you find!
+# Version: 2.0
+#
 #
 
-import requests
 import time
 import apikey
+import requests
 
-while True:
-	t = requests.get('http://blockchain.info/stats?format=json') #First request for blockchain data
-	jdatathen = t.json() #Read the returned data and assign it to a variable
-	then = jdatathen['market_price_usd'] #Grab market price at the time
-	time.sleep(1800) #30 Minute Delay
-	n = requests.get('http://blockchain.info/stats?format=json') #Make a second request for current information
-	jdatanow = n.json() 
-	now = jdatanow['market_price_usd'] #Grab current market price
-	if then-now >=100 or then-now <= -100: #Check for price difference
-		print "Difference Greater Than $100 Detected.... Yoing all Subscribers!"
-		yoall()
+x = True
 
-def yoall(): #Send YO!
-	request.post('https://api.justyo.co/yoall/', data={'api_token': apikey.api_token,'link': 'http://blockchain.info/charts/market-price'})
+#Get initial market value in United States Dollars
+def getvalue(): 
+	t = requests.get('https://blockchain.info/stats?format=json') 
+	jdatathen = t.json()
+	initial = jdatathen['market_price_usd']
+	print "First value request server response:"
+	print t # Print HTTP status code
+
+getvalue()
+
+def yo_all():
+	requests.post('http://api.justyo.co/yoall', data={'api_token': apikey.api_token,'link': 'https://blockchain.info/charts/market-price'}) # Send Yo to all
+	print "YO Sent"
+
+# Do while in Python
+while x == True:
+	time.sleep(3600) # One Hour
+	n = requests.get('https://blockchain.info/stats?format=json')
+	jdatanow = n.json()
+	now = jdatanow['market_price_usd']
+	if initial-now <= -1 or initial-now >= 1:
+		yo_all()
+		getvalue()
 	
