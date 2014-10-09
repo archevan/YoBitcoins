b12bcc50f7c3927f6af7e40d32541bb168c3978b
Evan Seils <evan@seils.cc>
2014-10-09 07:35:21 -0500

    Variable Scopes

--- YoBitcoins.py
+++ YoBitcoins.py
@@ -10,15 +10,18 @@ import time
 import apikey
 import requests
 
+initial = 0
 x = True
 
 #Get initial market value in United States Dollars
 def getvalue(): 
+	global initial
 	t = requests.get('https://blockchain.info/stats?format=json') 
 	jdatathen = t.json()
 	initial = jdatathen['market_price_usd']
 	print "First value request server response:"
 	print t # Print HTTP status code
+	print "Market Value in U.S. dollars" + initial
 
 getvalue()
 
@@ -32,7 +35,7 @@ while x == True:
 	n = requests.get('https://blockchain.info/stats?format=json')
 	jdatanow = n.json()
 	now = jdatanow['market_price_usd']
-	if initial-now <= -100 or initial-now >= 100:
+	if initial - now <= -100 or initial - now >= 100:
 		yo_all()
 		getvalue()
 	
