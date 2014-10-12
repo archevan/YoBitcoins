f606ed76f94a3674a9dadd18d2557e0bd2454e00
Ben Stobaugh <legoben1998@gmail.com>
2014-10-11 19:26:35 -0500

    Misc Cleanup

--- YoBitcoins.py
+++ YoBitcoins.py
@@ -11,7 +11,6 @@ import apikey
 import requests
 
 initial = 0
-x = True
 
 #Get initial market value in United States Dollars
 def getvalue(): 
@@ -21,22 +20,21 @@ def getvalue():
 	initial = jdatathen['market_price_usd']
 	print "First value request server response:"
 	print t # Print HTTP status code
-	print "Market Value in U.S. dollars" + initial
+	print "Market Value in U.S. dollars" + str(initial)
 
 getvalue()
-global initial
 
 def yo_all():
 	requests.post('http://api.justyo.co/yoall', data={'api_token': apikey.api_token,'link': 'https://blockchain.info/charts/market-price'}) # Send Yo to all
 	print "YO Sent"
 
 # Do while in Python
-while x == True:
-	time.sleep(3600) # One Hour
+while True:
 	n = requests.get('https://blockchain.info/stats?format=json')
 	jdatanow = n.json()
 	now = jdatanow['market_price_usd']
 	if initial - now <= -100 or initial - now >= 100:
 		yo_all()
 		getvalue()
+	time.sleep(3600) # One Hour
 	
